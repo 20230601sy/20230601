99f3f6108efbeb2cbe6ce229109717732ba51a8c
sy <20230601sy@gmail.com>
2023-11-02 13:21:06 +0900

    img_thumbnail

---
+++ 2_front_4_react/1102/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+function App() {
+  const IMG_PATH = 'https://20230601sy.github.io/0810/src/assets/';
+
+  return (
+    <div className="App">
+      <img src={`${IMG_PATH}thumbnail_sdk.png`} alt=""/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ 2_front_4_react/9998_sdk/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Table></Table>
+    </div>
+  );
+}
+
+export default App;
